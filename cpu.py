3fc8baa6f3d5c3e38985cfcd8bf935aab95af682
masonkarsevar <masonkarsevar@gmail.com>
2020-04-24 08:31:38 -0700

    removed some commented out code copied from the week project

--- cpu.py
+++ cpu.py
@@ -173,22 +173,7 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
+        
         with open(file_path, 'r', newline=None) as f:
             commands = f.readlines()
             new_commands = []
--- ls8.py
+++ ls8.py
@@ -5,41 +5,8 @@ import re
 import sys
 from cpu import *
 
-# write a load function that will take a file path 
-# as an argument file path is sys.argv[1]
-
-# def load(file_path):
-#     with open(file_path, 'r', newline=None) as f:
-#         commands = f.readlines()
-#         new_commands = []
-#         for instruction in commands:
-#             # print(instruction.rstrip('\n'))
-#             if instruction.startswith('0') or instruction.startswith('1'):
-#                 instruction_mod = instruction.split()
-#                 new_commands.append(instruction_mod[0])
-
-#     return new_commands
-
-# new_commands = load(sys.argv[1])
-
-# create a function that will iterate through the 
-# loaded in values from an external file and save them 
-# into the cpu's ram.
-    # use a for loop to iterate through the command array 
-        # use cpu.ram_write() to write the value into the 
-        # cpu's ram 
-
-# def add_to_ram(instructions, cpu):
-#     for address in range(len(instructions)):
-#         value = int(instructions[address], 2)
-#         # print(value)
-#         cpu.ram_write(value, address)
-
-# print(new_commands)
-
 cpu = CPU()
 
-# add_to_ram(new_commands, cpu)
 cpu.load(sys.argv[1])
 
 cpu.run()
